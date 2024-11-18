97884728afc67bbea435c78438bc471cc5de7e02
kung-fu-mari <ifcodes@duck.com>
2024-11-17 19:37:09 -0700

    Answer all part 3 prompts

--- comparisons.rb
+++ comparisons.rb
@@ -107,16 +107,18 @@ age = 1
 
 # YOU DO:
 # Determine if the dog loves to play and loves treats
-
+puts loves_to_play && loves_treats
 
 # Determine if the dog loves to play and loves the dog park
-
+puts loves_to_play && loves_dog_park
 
 # Determine if the dog loves to play or loves the dog park
-
+puts loves_to_play || loves_dog_park
 
 # Determine if the dog loves to play and is a puppy
 # Assume that a dog is a puppy if it is less than 2 years old.
-
+puts loves_to_play && age < 2
 # What did your final line of code evaluate to? Why do you think that is? Explain.
 # ANSWER:
+# It evaluated to true because loves_to_play is equal to true AND also age is 1, which is less than 2. 
+# Each comparison/variable on each side of the logical operator is true, so the whole statement evaluates to true.
